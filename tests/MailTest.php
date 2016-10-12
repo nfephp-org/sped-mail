058e6c07c0ba6e2564e94740634253fa49711c57
Roberto L. Machado <linux.rlm@gmail.com>
2016-10-12 09:35:08 -0300

    [UPD] Mail

---
+++ tests/MailTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace NFePHP\Mail\Tests;
+
+use NFePHP\Mail\Mail;
+
+class MailTest extends \PHPUnit_Framework_TestCase
+{
+    public $dummyMailer;
+    public $config;
+    
+    public function __construct()
+    {
+        $this->config = new \stdClass();
+        $this->config->host = 'smtp.test.com.br';
+        $this->config->user = 'usuario@test.com.br';
+        $this->config->password = 'senha';
+        $this->config->secure = 'tls';
+        $this->config->port = 587;
+        $this->config->from = 'usuario@test.com.br';
+        $this->config->fantasy = 'Test Ltda';
+        $this->config->replyTo = 'vendas@test.com.br';
+        $this->config->replyName = 'Vendas';
+        
+        $this->dummyMailer = $this->getMockBuilder('\PHPMailer')
+            ->setMethods(['send'])    
+            ->getMock();
+    }
+    
+    public function testSetTemplate()
+    {
+        $this->assertTrue(true);
+    }
+    
+    public function testLoadDocuments()
+    {
+        $this->assertTrue(true);
+    }
+    
+    public function testLOadDocumentsFail()
+    {
+        $this->assertTrue(true);
+    }
+    
+    public function testSend()
+    {
+        $this->assertTrue(true);
+    }
+    
+    public function testSendFail()
+    {
+        $this->assertTrue(true);
+    }
+}
